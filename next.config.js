c81e4bd63ae160c491d4a9ec531445482b814a27
Nicola D'Aniello <nicola.danie@gmail.com>
2019-08-08 17:54:37 +0100

    install next-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
 const withSass = require("@zeit/next-sass");
 module.exports = withSass();
+
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+module.exports = withBundleAnalyzer({
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html"
+    }
+  }
+});
