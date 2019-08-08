9f05f1be92dbf9a4069a22c9a710d10d98a62a5e
Nicola D'Aniello <nicola.danie@gmail.com>
2019-08-08 21:58:11 +0100

    add description to meta tags

--- components/common/Layout/index.js
+++ components/common/Layout/index.js
@@ -1,20 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import Head from "next/head";
-import Footer from "../../Footer/index.js";
-import Header from "../../Header/index.js";
+import SettingsContext from "../../../providers/SettingsContext.js";
 import LayoutSidebar from "../../LayoutSidebar/index.js";
 import { stripHtml } from "../../../util.js";
+import Header from "../../Header/index.js";
+import Footer from "../../Footer/index.js";
 
-const Layout = ({ title, page = {}, children }) => (
-  <Fragment>
-    <Head>
-      {title ? (
-        <title>{title}</title>
-      ) : page.title ? (
-        <title>{stripHtml(page.title)}</title>
-      ) : null}
-      <style>
-        {`
+const Layout = ({ title, description, page = {}, children }) => {
+  const { generalSettingsDescription } = useContext(SettingsContext);
+
+  return (
+    <Fragment>
+      <Head>
+        {title ? (
+          <title>{title}</title>
+        ) : page.title ? (
+          <title>{stripHtml(page.title)}</title>
+        ) : null}
+        {!!generalSettingsDescription && (
+          <meta name="description" content={generalSettingsDescription} />
+        )}
+        <style>
+          {`
       @font-face {
         font-family: 'Foo';
         src: url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
@@ -24,28 +31,29 @@ const Layout = ({ title, page = {}, children }) => (
         font-family: 'Montserrat', sans-serif;
       }
     `}
-      </style>
-    </Head>
-    <Header page={page} />
-    <main className="py-4">
-      <div className="container">
-        <div className="row justify-content-around">
-          <div className="col-md-8 col-xs-12">
-            {!!page && !!page.content && (
-              <section>
-                <div dangerouslySetInnerHTML={{ __html: page.content }} />
-              </section>
-            )}
-            {children}
-          </div>
-          <div className="col">
-            <LayoutSidebar />
+        </style>
+      </Head>
+      <Header page={page} />
+      <main className="py-4">
+        <div className="container">
+          <div className="row justify-content-around">
+            <div className="col-md-8 col-xs-12">
+              {!!page && !!page.content && (
+                <section>
+                  <div dangerouslySetInnerHTML={{ __html: page.content }} />
+                </section>
+              )}
+              {children}
+            </div>
+            <div className="col">
+              <LayoutSidebar />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-    <Footer />
-  </Fragment>
-);
+      </main>
+      <Footer />
+    </Fragment>
+  );
+};
 
 export default Layout;
