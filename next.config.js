d4698e049672a972524f557c84651bf81f63a483
Nicola D'Aniello <nicola.danie@gmail.com>
2019-08-08 21:50:26 +0100

    withOptimizedImages installed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
+const withOptimizedImages = require("next-optimized-images");
 
 module.exports = withCSS(
-  withSass(
-    withPurgeCss({
-      purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer
-    })
+  withOptimizedImages(
+    withSass(
+      withPurgeCss({
+        purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer
+      })
+    )
   )
 );
