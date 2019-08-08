0d4a13d7c718951a7cea2537379f14dc11b14146
Nicola D'Aniello <nicola.danie@gmail.com>
2019-08-08 20:13:42 +0100

    essentials plugins for prod

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 const withSass = require("@zeit/next-sass");
-module.exports = withSass();
+const withCSS = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
+
+module.exports = withCSS(
+  withSass(
+    withPurgeCss({
+      purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer
+    })
+  )
+);
